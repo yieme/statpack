8b83c266d718ba6b08713fce95ba1500a8e27435
yieme <yieme@cormea.com>
2015-02-06 21:23:03 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# statpack
+# statpack <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="120">
 Process Statistic Package
+
+# Install
+
+
+```js
+npm install statpack --save
+```
+
+# License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// statpack - Process Statistic Package - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var Statpack = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = Statpack // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return Statpack }) // support AMD
+  // else this.Statpack = Statpack // support browser
+})();
