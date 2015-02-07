c93656534907a299afa8366b8e30221d31af1023
yieme <yieme@cormea.com>
2015-02-06 21:42:17 -0700

    initial

--- index.js
+++ index.js
@@ -3,10 +3,74 @@
 ;(function() {
   'use strict';
 
-  // var convar = require('convar')
+  var os              = require('os')
+    , ss              = require('simple-statistics')
 
-  var Statpack = function() {
+  var Statpack = function(timeFunction) {
+    timeFunction = timeFunction || (new Date()).valueOf
+    var self = this
+      , stats = {
+        use  : [],
+        tim  : [],
+        tmem : [],
+        fmem : [],
+        load : []
+      }
+      , MAX_STATS       = 100
+      , statAreas       = ['use', 'tim', 'tmem', 'fmem', 'load']
+      , iterations      = 0
+      , duration        = 0
+      , timerBegin      = 0
+    ;
 
+    self.shiftStats = function shiftStats() {
+      for (var i = 0; i < statAreas.length; i++) {
+        var area = statAreas[i]
+        stats[area].shift()
+      }
+    }
+
+    self.getStats = function getStats() {
+      var result = {}
+      for (var i = 0; i < statAreas.length; i++) {
+        var area = statAreas[i]
+        var stat = stats[area]
+        result[area] = {
+          min: ss.min(stat),
+          max: ss.max(stat),
+          ave: ss.average(stat)
+        }
+      }
+      return result
+    }
+
+    self.addStats = function addStats() {
+      if (stats.use.length >= MAX_STATS) self.shiftStats()
+      stats.use.push(iterations)
+      var averageDuration = (duration) ? Math.round(duration / iterations) : 0
+      stats.tim.push(averageDuration)
+      iterations = 0
+      duration   = 0
+      stats.tmem.push(os.totalmem())
+      stats.fmem.push(os.freemem())
+      var load = Math.round(os.loadavg()[0])
+      stats.load.push(load)
+    }
+
+    self.addIteration = function addIteration(time) {
+      iterations++
+      duration += time
+    }
+
+    self.beginOp = function beginOp() {
+      timerBegin = timeFunction()
+    }
+
+    self.endOp = function endOp() {
+      self.addIteration(timeFunction() - timerBegin)
+    }
+
+    return self
   }
 
 
